2816089e89206f0c040922e9302786aa1e2bac90
kento_morota <kento.0109k@gmail.com>
2016-08-19 15:14:51 +0900

    implement alpha version

--- mys_disp.rb
+++ mys_disp.rb
@@ -10,11 +10,18 @@ today = DateTime.now.to_s.slice(0..9)
 begin
 
   db = SQLite3::Database.open "mys.sqlite3"
-  stm = db.prepare "SELECT * FROM mys WHERE datetime <= '#{today}' LIMIT 7"
+  stm = db.prepare "SELECT * FROM mys WHERE datetime <= '#{today}' ORDER BY datetime LIMIT 7"
   rs = stm.execute
 
   rs.each do |row|
-    p row
+    print row[1].slice(5..9)
+    print " : "
+    # display * every 20 munitues
+    disptime = row[2] / 20
+    disptime.times do 
+      print "*"
+    end
+    puts ""
   end
 
 rescue SQLite3::Exception => e
